03bd23db7d700eaa7d031c6d0cf528dd520d24c3
WofWca <wofwca@gmail.com>
2019-02-08 13:02:00 +0800

    Updates

---
+++ precache-manifest.db6d5c9cb5b64408faf80cbeb7f7e2b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc3553b17436a8a7aec4",
+    "url": "/react-life/static/js/main.dc3553b1.chunk.js"
+  },
+  {
+    "revision": "cc4b32316d6007561a9c",
+    "url": "/react-life/static/js/1.cc4b3231.chunk.js"
+  },
+  {
+    "revision": "93d02bacbfb5e40aa9ee",
+    "url": "/react-life/static/js/runtime~main.93d02bac.js"
+  },
+  {
+    "revision": "0c5a53402a874b6033eefcc7374d4bf0",
+    "url": "/react-life/index.html"
+  }
+];
