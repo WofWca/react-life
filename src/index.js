6181ba4cca28a3066a2f6eb60673636da20bb901
WofWca <wofwca@gmail.com>
2019-02-08 14:32:00 +0800

    Refactor
    snake_case -> camelCase

--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ class Grid extends React.Component {
     return (
       <table style={{borderCollapse: "collapse"}}>
         <tbody>
-          {this.props.cells.map((row, row_i) => (
-            <tr className="grid_row" key={`${row_i}`}>
-              {row.map((cell_val, column_i) => (
-                <Cell alive={cell_val} key={`${row_i}:${column_i}`}/>
+          {this.props.cells.map((row, rowI) => (
+            <tr className="gridRow" key={`${rowI}`}>
+              {row.map((cellVal, columnI) => (
+                <Cell alive={cellVal} key={`${rowI}:${columnI}`}/>
               ))}
             </tr>
           ))}
@@ -45,41 +45,41 @@ class Game extends React.Component {
       // TODO replace with 1D?
       cells: props.cells, // A 2D array of bool.
     };
-    this.generation_num = 0;
+    this.generationNum = 0;
     this.frequency = "frequency" in props ? props.frequency : 2;  // In seconds
-    this.grid_height = props.cells.length;
-    this.grid_width = props.cells[0].length;
-    this.next_gen_cells = [];
-    this.next_gen_cells.length = this.grid_height;
-    for (let row_i = 0; row_i < this.next_gen_cells.length; row_i++) {
-      let new_row = []; new_row.length = this.grid_width;
-      new_row.fill(false, 0, new_row.length);
-      this.next_gen_cells[row_i] = new_row;
+    this.gridHeight = props.cells.length;
+    this.gridWidth = props.cells[0].length;
+    this.nextGenCells = [];
+    this.nextGenCells.length = this.gridHeight;
+    for (let rowI = 0; rowI < this.nextGenCells.length; rowI++) {
+      let newRow = []; newRow.length = this.gridWidth;
+      newRow.fill(false, 0, newRow.length);
+      this.nextGenCells[rowI] = newRow;
     }
   }
 
-  cell_is_alive(row_i, column_i) {
-    if (row_i < 0 || row_i >= this.grid_height ||
-      column_i < 0 || column_i >= this.grid_width) {
+  cellIsAlive(rowI, columnI) {
+    if (rowI < 0 || rowI >= this.gridHeight ||
+      columnI < 0 || columnI >= this.gridWidth) {
       return false;
     }
-    if (this.state.cells[row_i][column_i] === false) {
+    if (this.state.cells[rowI][columnI] === false) {
       return false;
     }
     return true;
   }
 
-  cell_get_num_neighbors (row_i, column_i) { 
-    let num_neighbors = 0;
-    if (this.cell_is_alive(row_i - 1, column_i - 1)) num_neighbors++;
-    if (this.cell_is_alive(row_i - 1, column_i)) num_neighbors++;
-    if (this.cell_is_alive(row_i - 1, column_i + 1)) num_neighbors++;
-    if (this.cell_is_alive(row_i, column_i - 1)) num_neighbors++;
-    if (this.cell_is_alive(row_i, column_i + 1)) num_neighbors++;
-    if (this.cell_is_alive(row_i + 1, column_i - 1)) num_neighbors++;
-    if (this.cell_is_alive(row_i + 1, column_i)) num_neighbors++;
-    if (this.cell_is_alive(row_i + 1, column_i + 1)) num_neighbors++;
-    return num_neighbors;
+  cellGetNumNeighbors (rowI, columnI) { 
+    let numNeighbors = 0;
+    if (this.cellIsAlive(rowI - 1, columnI - 1)) numNeighbors++;
+    if (this.cellIsAlive(rowI - 1, columnI)) numNeighbors++;
+    if (this.cellIsAlive(rowI - 1, columnI + 1)) numNeighbors++;
+    if (this.cellIsAlive(rowI, columnI - 1)) numNeighbors++;
+    if (this.cellIsAlive(rowI, columnI + 1)) numNeighbors++;
+    if (this.cellIsAlive(rowI + 1, columnI - 1)) numNeighbors++;
+    if (this.cellIsAlive(rowI + 1, columnI)) numNeighbors++;
+    if (this.cellIsAlive(rowI + 1, columnI + 1)) numNeighbors++;
+    return numNeighbors;
   }
 
   step () {
@@ -87,31 +87,31 @@ class Game extends React.Component {
     // TODO only calcualte cells whose neighbours have changed.
 
     // Copy the from the old state.
-    // You can't just `this.next_gen_cells = this.state.cells.slice()`, as this is
+    // You can't just `this.nextGenCells = this.state.cells.slice()`, as this is
     // a shallow copy.
-    for (let row_i = 0; row_i < this.next_gen_cells.length; row_i++) {
-      this.next_gen_cells[row_i] = this.state.cells[row_i].slice();
+    for (let rowI = 0; rowI < this.nextGenCells.length; rowI++) {
+      this.nextGenCells[rowI] = this.state.cells[rowI].slice();
     }
 
     // TODO these forEach(..., this) are ugly.
-    this.state.cells.forEach((row, row_i) => {
-      row.forEach((curr_cell_alive, column_i) => {
+    this.state.cells.forEach((row, rowI) => {
+      row.forEach((currCellAlive, columnI) => {
         // Now, the actual game logic
-        let num_neighbors = this.cell_get_num_neighbors(row_i, column_i);
+        let numNeighbors = this.cellGetNumNeighbors(rowI, columnI);
         if (
-          num_neighbors === 3 ||
-          (num_neighbors === 2 && curr_cell_alive)
+          numNeighbors === 3 ||
+          (numNeighbors === 2 && currCellAlive)
         ) {
-          this.next_gen_cells[row_i][column_i] = true;
+          this.nextGenCells[rowI][columnI] = true;
         } else {
-          this.next_gen_cells[row_i][column_i] = false;
+          this.nextGenCells[rowI][columnI] = false;
         }
       }, this);
     }, this);
     // Exchange the current state and the buffer for the next one.
-    let old_cells_gen_arr = this.state.cells;
-    this.setState({ cells: this.next_gen_cells });
-    this.next_gen_cells = old_cells_gen_arr;
+    let coldCellsGenArr = this.state.cells;
+    this.setState({ cells: this.nextGenCells });
+    this.nextGenCells = coldCellsGenArr;
   }
 
   render() {
