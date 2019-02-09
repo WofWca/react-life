d6cff4c0a2f818a606b3428713176ef3dae0bd7f
WofWca <wofwca@gmail.com>
2019-02-09 16:15:44 +0800

    Updates

---
+++ precache-manifest.50a3b2b4e88f19d80236dbef4afee80d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bee20a88e799cd9454",
+    "url": "/react-life/static/js/main.79bee20a.chunk.js"
+  },
+  {
+    "revision": "998c1f1de09a2386881b",
+    "url": "/react-life/static/js/1.998c1f1d.chunk.js"
+  },
+  {
+    "revision": "93d02bacbfb5e40aa9ee",
+    "url": "/react-life/static/js/runtime~main.93d02bac.js"
+  },
+  {
+    "revision": "236296caa927e526f9f022ba93845a2c",
+    "url": "/react-life/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-life/precache-manifest.e6c3e80eaedcd458e9bf818b756cceb9.js"
+  "/react-life/precache-manifest.50a3b2b4e88f19d80236dbef4afee80d.js"
 );
 
 workbox.clientsClaim();
